6a8ce4ba0cdf431ee85461dcbd2d7a229189a1b8
unknown <christoph02.cw@gmail.com>
2022-02-08 11:04:59 +0100

    :bug:

--- src/backend/dataTypes.py
+++ src/backend/dataTypes.py
@@ -1,3 +1,3 @@
 #Day
 def Day(date: str,text1:str,text2:str,id=None) -> list:
-    return {"id":id,"date":date,"dataChris":text1,"dataKay":text2}
+    return {"id":id,"date":date,"data2":text1,"data1":text2}
